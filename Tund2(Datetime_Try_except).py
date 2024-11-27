c94e6d45181b0157a4d00e0eab70138d2dfe2d0a
opilane <opilane@HP-CZC2349HT2>
2024-11-27 16:44:13 +0200

    Tund 2

--- Tund2(Datetime_Try_except).py
+++ Tund2(Datetime_Try_except).py
@@ -1,7 +1,8 @@
 from datetime import *
 from calendar import *
 from random import *
-import math
+#import math
+from math import *
 
 # tana=date.today() #nimetus() - funktsioon
 # tanaf=date.today().strftime("%B %d, %Y")
@@ -50,31 +51,31 @@ import math
 # Ulesanne 3 ##
 
 #1 variant
-try:
-    r=int(input("Ruudu sees asub ring. Ringi raadius on R. Sisesta R: "))
-    ruudu_pindala=4*r**2
-    ruudu_umbermoot=4*r
-    ringi_pindala=math.pi*r
-    ringi_umbermoot=math.pi*2*r
+# try:
+#     r=int(input("Ruudu sees asub ring. Ringi raadius on R. Sisesta R: "))
+#     ruudu_pindala=4*r**2
+#     ruudu_umbermoot=4*r
+#     ringi_pindala=math.pi*r
+#     ringi_umbermoot=math.pi*2*r
       
-    print(f"Ringi pindala on {ringi_pindala}\nRingi ümbermõõt on {ringi_umbermoot}\nRuudu pindala on {ruudu_pindala}\nRuudu ümbermõõt on {ringi_umbermoot}\n")
+#     print(f"Ringi pindala on {ringi_pindala}\nRingi ümbermõõt on {ringi_umbermoot}\nRuudu pindala on {ruudu_pindala}\nRuudu ümbermõõt on {ringi_umbermoot}\n")
 
-    # print(ruudu_pindala)
-    # print(ruudu_umbermoot)
-    # print(ringi_pindala)
-    # print(ringi_umbermoot)
-except:
-    print("On vaja numbreid sisestada!")
+#     # print(ruudu_pindala)
+#     # print(ruudu_umbermoot)
+#     # print(ringi_pindala)
+#     # print(ringi_umbermoot)
+# except:
+#     print("On vaja numbreid sisestada!")
 
 #2 variant
-R=round(random()*100) #0.0...1.0
-print(f"R={R}")
-ruudu_pindala=4*R**2
-ruudu_umbermoot=4*R
-ringi_pindala=math.pi*R
-ringi_umbermoot=math.pi*2*R
+# R=round(random()*100) #0.0...1.0
+# print(f"R={R}")
+# ruudu_pindala=4*R**2
+# ruudu_umbermoot=4*R
+# ringi_pindala=math.pi*R
+# ringi_umbermoot=math.pi*2*R
 
-print(f"Ringi pindala on {ringi_pindala}\nRingi ümbermõõt on {ringi_umbermoot}\nRuudu pindala on {ruudu_pindala}\nRuudu ümbermõõt on {ringi_umbermoot}\n")
+# print(f"Ringi pindala on {ringi_pindala}\nRingi ümbermõõt on {ringi_umbermoot}\nRuudu pindala on {ruudu_pindala}\nRuudu ümbermõõt on {ringi_umbermoot}\n")
 
 ###############
 
@@ -82,3 +83,9 @@ print(f"Ringi pindala on {ringi_pindala}\nRingi ümbermõõt on {ringi_umbermoot
 
 ## Ulesanne 4 ##
 
+d=2.575 # mündi d
+maa=6378 # maa radius km
+maa*=100000 # maa radius sm
+Lmaa=2*pi*maa
+kogus=int(Lmaa/d)
+print(f"On vaja {kogus} mündi.\nMeil on vaja {kogus*2} eur")
